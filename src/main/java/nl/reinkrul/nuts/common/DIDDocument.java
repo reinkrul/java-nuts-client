c4c01caee213444bcaa58122d136e72143fef00a
Rein Krul <info@reinkrul.nl>
2023-05-12 15:27:06 +0200

    disable URL query marshalling

--- src/main/java/nl/reinkrul/nuts/common/DIDDocument.java
+++ src/main/java/nl/reinkrul/nuts/common/DIDDocument.java
@@ -2,10 +2,4 @@ package nl.reinkrul.nuts.common;
 
 public class DIDDocument extends foundation.identity.did.DIDDocument {
 
-    /**
-     * Required by OpenAPI Generator Tools v6.6.0, but not used in practice.
-     */
-    public String toUrlQueryString(String param) {
-        return null;
-    }
 }
